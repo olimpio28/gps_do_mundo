0573d6b7023c828e3271eff620d23ed413eef8c5
Max_Wilian <maxwilian01@gmail.com>
2023-03-30 15:51:06 +0000

    add código novo

--- lib/card.1.dart
+++ lib/card.1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'gpsdomundo_theme.dart';
 
 class Card1 extends StatelessWidget {
   const Card1({super.key});
@@ -13,29 +14,44 @@ class Card1 extends StatelessWidget {
   Widget build(BuildContext context) {
     //3
     return Center(
-        //todo: card1 decorate Container
-        child: Container(
-      // 1
-      padding: const EdgeInsets.all(16),
-      // 2
-      constraints: const BoxConstraints.expand(
-        width: 350,
-        height: 450,
-      ),
-      // 3
-      decoration: const BoxDecoration(
-        //4
-        image: DecorationImage(
-          //5
-          image: NetworkImage(
-              'https://blog.123nilhas.com/wp-content/uploads/2022/12/conheca-os-lugares-com-as-melhores-vistas-do-rio-de-janeiro-conexao123.jpg'),
-          //6
-          fit: BoxFit.cover,
+      //todo: card1 decorate Container
+      child: Container(
+        // 1
+        padding: const EdgeInsets.all(16),
+        // 2
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
         ),
-        //7
-        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        // 3
+        decoration: const BoxDecoration(
+          //4
+          image: DecorationImage(
+            //5
+            image: NetworkImage(
+                'https://blog.123nilhas.com/wp-content/uploads/2022/12/conheca-os-lugares-com-as-melhores-vistas-do-rio-de-janeiro-conexao123.jpg'),
+            //6
+            fit: BoxFit.cover,
+          ),
+          //7
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        child: Stack(children: [
+          Text(
+            category,
+            style: GpsdoMundoTheme.darkTextTheme.bodyText1,
+          ),
+          Text(title, style: GpsdoMundoTheme.darkTextTheme.headline5),
+          Text(
+            description,
+            style: GpsdoMundoTheme.darkTextTheme.bodyText1,
+          ),
+          Text(
+            tourist,
+            style: GpsdoMundoTheme.darkTextTheme.bodyText1,
+          )
+        ]),
       ),
-      //TODO: Adicionar uma pilha de texto
-    ));
+    );
   }
 }
