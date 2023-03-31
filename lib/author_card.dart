920c7a8a2aa47838ba38d3ed81034b43ff8b73a5
Max_Wilian <maxwilian01@gmail.com>
2023-03-31 17:40:05 +0000

    add arquivo e códigos novos.

---
+++ lib/author_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'gpsdomundo_theme.dart';
+import 'circle.image.dart';
+
+class AuthorCard extends StatelessWidget {
+  //1
+  final String authorName;
+  final String title;
+  final ImageProvider? imageProvider;
+
+  const AuthorCard({
+    super.key,
+    required this.authorName,
+    required this.title,
+    this.imageProvider,
+  });
+  //2
+  @override
+  Widget build(BuildContext context) {
+    //TODO: Substitua return container(...) por...
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
